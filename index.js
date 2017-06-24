5299c9ddff760bafc5e20b5fddcb10ef40b86303
Danie du Plessis <danie@gofuseit.com>
2017-06-24 12:15:28 +0200

    version 1

--- index.js
+++ index.js
@@ -16,6 +16,8 @@
 //     }
 // ],
 //
+// When you attempt to add a device, it will ask for a "PIN code".
+// The default code for all HomeBridge accessories is 031-45-154.
 
 /*
 // The value property of SecuritySystemCurrentState must be one of the following:
@@ -24,14 +26,16 @@ Characteristic.SecuritySystemCurrentState.AWAY_ARM = 1;
 Characteristic.SecuritySystemCurrentState.NIGHT_ARM = 2;
 Characteristic.SecuritySystemCurrentState.DISARMED = 3;
 Characteristic.SecuritySystemCurrentState.ALARM_TRIGGERED = 4;
+
 // The value property of SecuritySystemTargetState must be one of the following:
 Characteristic.SecuritySystemTargetState.STAY_ARM = 0;
 Characteristic.SecuritySystemTargetState.AWAY_ARM = 1;
 Characteristic.SecuritySystemTargetState.NIGHT_ARM = 2;
 Characteristic.SecuritySystemTargetState.DISARM = 3;
 */
+'use strict';
 
-var Service, Characteristic;
+var	 Service, Characteristic, informationService;
 var mqtt = require("mqtt");
 
 function MqttAlarmAccessory(log, config) {
@@ -58,18 +62,22 @@ function MqttAlarmAccessory(log, config) {
     	rejectUnauthorized: false
 	};
 	this.caption		= config["caption"];
-	this.topicStatusCurrent	= config["topics"].statusCurrent; // the target value sent to HomeKit
-	this.topicStatusTarget	= config["topics"].statusTarget; // the actual value for door state
+	this.topicStatusCurrent	= config["topics"].statusCurrent; // the current value sent to HomeKit
+	this.topicButtonPress	= config["topics"].buttonPress; // arm/disarm alarm
 
-	this.CachedAlarmCurrentState = null; // Characteristic.CurrentDoorState.CLOSED; // 1 = closed
-	this.CachedAlarmTargetState = null; //Characteristic.CurrentDoorState.CLOSED; // 1 = closed   
+	this.CachedAlarmCurrentState = 3;
+	this.CachedAlarmTargetState = 3;    
     
 	this.service = new Service.SecuritySystem(this.name); // this is the service type from the HomeKitTypes.js file https://github.com/KhaosT/HAP-NodeJS/blob/master/lib/gen/HomeKitTypes.js
-    
-    this.service.getCharacteristic(Characteristic.SecuritySystemCurrentState).on('get', this.getAlarmCurrentState.bind(this));
+	this.service.getCharacteristic(Characteristic.SecuritySystemCurrentState).on('get', this.getAlarmCurrentState.bind(this));
     this.service.getCharacteristic(Characteristic.SecuritySystemTargetState).on('get',this.getAlarmTargetState.bind(this));
-    
     this.service.getCharacteristic(Characteristic.SecuritySystemTargetState).on('set',this.setAlarmTargetState.bind(this));
+	
+	this.informationService = new Service.AccessoryInformation();
+	this.informationService.setCharacteristic(Characteristic.Manufacturer, "Danie du Plessis")
+	this.informationService.setCharacteristic(Characteristic.Model, "Sonoff SV Alarm Connection")
+	this.informationService.setCharacteristic(Characteristic.SerialNumber, "1.0.0")
+    
 	
 	// connect to MQTT broker
 	this.client = mqtt.connect(this.url, this.options);
@@ -78,24 +86,44 @@ function MqttAlarmAccessory(log, config) {
 		that.log('Error event on MQTT');
 	});
 	this.client.on('message', function (topic, message) {
-        that.log( "Got MQTT! alarm" );
+        that.log( "Got MQTT! alarm " + topic + ' ' + message);
 		if (topic == that.topicStatusCurrent) { // actual value changed
 			var statusCurrent = parseInt(message);
-			that.CachedGarageDoorState = statusCurrent;
+			that.CachedAlarmCurrentState = statusCurrent;
             that.service.getCharacteristic(Characteristic.SecuritySystemCurrentState).setValue(statusCurrent, undefined, 'fromSetValue');
             }
-        if (topic == that.topicStatusTarget) { // target value changed
+        if (topic == that.topicButtonPress) { // target value changed
 			var statusTarget = parseInt(message);
-            if (that.CachedGarageTargetDoorState != statusTarget) { // avoid loopback from own changes
-			that.CachedGarageTargetDoorState = statusTarget;
-            that.service.getCharacteristic(Characteristic.SecuritySystemTargetState).setValue(statusTarget, undefined, 'fromSetValue');
+            if (that.CachedAlarmTargetState != statusTarget) { // avoid loopback from own changes
+			that.CachedAlarmTargetState = statusTarget;
+			that.CachedAlarmCurrentState = statusTarget;
+			if (statusTarget < 3){
+				that.service.getCharacteristic(Characteristic.SecuritySystemTargetState).setValue(1, undefined, 'fromSetValue');
+				wait(500);
+				that.client.publish(that.topicbuttonPress, String(1));
+			}
+			else
+			{
+				that.service.getCharacteristic(Characteristic.SecuritySystemTargetState).setValue(0, undefined, 'fromSetValue');
+				wait(500);
+				that.client.publish(that.topicbuttonPress, String(0));
+			}            
             }
 		}
 	});
     
     //the topics to subscribe to, this is set in the config file
     this.client.subscribe(this.topicStatusCurrent);
-    this.client.subscribe(this.topicStatusTarget);
+    this.client.subscribe(this.topicButtonPress);
+	
+	
+	function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
 }
 
 module.exports = function(homebridge) {
@@ -105,23 +133,23 @@ module.exports = function(homebridge) {
 };
 
 MqttAlarmAccessory.prototype.getAlarmCurrentState = function(callback) {
-    this.log("getAlarmCurrentState");
+    this.log("getAlarmCurrentState " + this.CachedAlarmCurrentState);
     callback(null, this.CachedAlarmCurrentState);
 };
 
 MqttAlarmAccessory.prototype.getAlarmTargetState = function(callback) {
-    this.log("getAlarmTargetState");
+    this.log("getAlarmTargetState " + this.CachedAlarmTargetState);
     callback(null, this.CachedAlarmTargetState);
 };
 
 MqttAlarmAccessory.prototype.setAlarmTargetState = function(status, callback) {
     this.log("setAlarmTargetState");
     this.CachedAlarmTargetState = status;
-	this.client.publish(this.topicStatusTarget, String(status) ); // send MQTT packet for new state
+	this.client.publish(this.topicButtonPress, String(status) ); // send MQTT packet for new state
 	callback();
 };
 
 
 MqttAlarmAccessory.prototype.getServices = function() {
-  return [MqttAlarmAccessory.service];
+  return [this.informationService, this.service];
 };
